1f4f0b6908c38221e5215ee443c1189dd97ee445
geotown78-dev <geotown78@gmail.com>
2026-05-16 03:50:49 +0400

    Delete src directory

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from './lib/supabase';
-
-export const MOCK_EVENTS: Event[] = [];
-
-export const SCHEDULE: any[] = [];
-
-export const ADMIN_EMAILS = ['georgetchedia74@gmail.com'];
